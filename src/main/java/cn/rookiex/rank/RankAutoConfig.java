e570ac1d021d38fd536ad909f284ddc3d3239bbe
rookiex <774590465@qq.com>
2019-09-05 17:51:07 +0800

    自动配置

---
+++ src/main/java/cn/rookiex/rank/RankAutoConfig.java
@@ -0,0 +1,20 @@
+package cn.rookiex.rank;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * @Author : Rookiex
+ * @Date : Created in 2019/9/5 17:50
+ * @Describe :
+ * @version: 1.0
+ */
+@Configuration
+public class RankAutoConfig {
+    @Bean
+    StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
